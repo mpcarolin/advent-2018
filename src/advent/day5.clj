f671d1ea5bece6e24bce563d9f47a0e2f4d3ca67
mpcarolin <mpcarolin@gmail.com>
2019-01-26 19:02:12 -0700

    vastly improved runtime performacne of part1, then solved part2.

--- src/advent/day5.clj
+++ src/advent/day5.clj
@@ -2,7 +2,7 @@
   (:require [advent.core :as core]
             [clojure.string :as s]))
 
-(def polymers (core/get-input 5))
+(def polymers (first (core/get-input 5)))
 
 ;; ======== PART 1 ======== 
 
@@ -20,30 +20,51 @@
 
 (defn reacts?
   "Returns true if the pair of polymer units react"
-  [pair]
-  (and (apply same-type? pair)
-       (apply opposite-polarity? pair)))
+  [a b]
+  (and (not-any? nil? [a b])
+       (apply same-type? [a b])
+       (apply opposite-polarity? [a b])))
 
-(defn char-vec [s] (map identity s))
-(defn vec->str [v] (apply str v))
+(defn react-reducer
+  [finished unit]
+  (if-let [prev (peek finished)]
+    (if (reacts? unit prev)
+      (pop finished)
+      (conj finished unit))
+    [unit]))
 
-(defn react-polymer-once
-  [polymers]
-  (loop [[a b & rest] (char-vec polymers)
-         result []]
-    (cond
-      (nil? a) (vec->str result)
-      (nil? b) (vec->str (conj result a))
-      (reacts? [a b]) (recur rest result)
-      :else (recur (cons b rest)
-                   (conj result a)))))
+(defn react [polymer] (reduce react-reducer [] polymer))
 
-(defn react-polymers
-  [polymers]
-  (loop [polys polymers]
-    (let [result (react-polymer-once polys)]
-      (if (= result polys)
-        polys
-        (recur result)))))
+(def part1-result
+  (let [reacted (react polymers)]
+    {:reacted reacted
+     :count (count reacted)}))
 
 ;; ======== PART 2 ======== 
+
+(defn chars-of
+  [unit]
+  (conj #{}
+        (Character/toLowerCase unit)
+        (Character/toUpperCase unit)))
+
+(defn remove-unit
+  "Returns s with unit removed"
+  [s unit]
+  (->> s
+      (remove (chars-of unit))
+      (apply str)))
+
+(defn find-optimal-reaction
+  "Removes the unit type from s that is blocking the most reactions and then reacts the result."
+  [polymers]
+  (let [units (set (s/lower-case polymers))
+        permutations (map #(remove-unit polymers %) units)
+        reactions (map react permutations)]
+    (apply min-key count reactions)))
+
+(def part2-result
+  (let [result (find-optimal-reaction polymers)]
+    {:result result
+     :count (count result)}))
+
